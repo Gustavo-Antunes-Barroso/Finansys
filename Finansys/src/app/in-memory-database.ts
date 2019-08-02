98b1d043b9ff05f65423baa7a637624f68287213
Gustavo-Antunes-Barroso <ads.gustavo94@gmail.com>
2019-08-02 11:25:53 -0300

    Foi criado o tipo Category

--- Finansys/src/app/in-memory-database.ts
+++ Finansys/src/app/in-memory-database.ts
@@ -1,15 +1,18 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
+import { Category } from "./pages/categories/shared/category.model"
+
 export class InMemoryDatabase implements InMemoryDbService{
 
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             { id: 1, name: 'Moradia', description: "Pagamentos de contas de casa" },
             { id: 2, name: 'Saúde', description: "Plano de saúde e remédios" },
             { id: 3, name: 'Lazer', description: "Cinema, parques, viagens, etc" },
             { id: 4, name: 'Salário', description: "Recebimento do salário" },
             { id: 5, name: 'Freelas', description: "Trabalhos freelancer" }
         ];
+        
         return { categories }
     }
 
